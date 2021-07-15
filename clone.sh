2bfa51c266935b1395bd2fc6e405eea4b705c05f
Maximilian Dewald <maximilian@dewaldnet.de>
2021-07-15 13:33:59 +0200

    Fixed db restore not working
    other try of fixing restore
    
    
    another try
    
    
    another one
    
    
    final fix?

--- clone.sh
+++ clone.sh
@@ -5,4 +5,4 @@ MONGO_URI_TARGET=$2
 DB_NAME_SOURCE="${MONGO_URI_SOURCE##*/}"
 DB_NAME_TARGET="${MONGO_URI_TARGET##*/}"
 
-mongodump --uri "${MONGO_URI_SOURCE}" --forceTableScan --archive | mongorestore --uri "${MONGO_URI_TARGET}" --archive --drop --nsFrom="${DB_NAME_SOURCE}.*" --nsTo="${DB_NAME_TARGET}.*"
+mongodump --uri="${MONGO_URI_SOURCE}" --forceTableScan --archive | mongorestore --uri="${MONGO_URI_TARGET}" --archive --nsInclude="${DB_NAME_SOURCE}.*" --nsFrom="${DB_NAME_SOURCE}.*" --nsTo="${DB_NAME_TARGET}.*" --drop
